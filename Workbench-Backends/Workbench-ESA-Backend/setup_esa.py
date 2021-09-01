22a566aff3551dc3af05a04fd46960e3743a154f
Cleo Schulten <cs@rias-institute.eu>
2021-09-01 14:04:02 +0200

    allow for some preloading

--- Workbench-Backends/Workbench-ESA-Backend/setup_esa.py
+++ Workbench-Backends/Workbench-ESA-Backend/setup_esa.py
@@ -30,17 +30,19 @@ esa_db_path = config_esa.esa_db_path
 
 esa_cutoffs = config_esa.esa_cutoffs
 
+research_areas_esa = ClassificationESA(esa_db_path, host, user, password, database_research_areas)
+research_areas = research_areas_esa.get_classification_areas()
+research_area_wikis = research_areas_esa.get_classification_area_wikis()
+
+sdg_esa = ClassificationESA(esa_db_path, host, user, password, database_sdgs)
+sdg_areas = sdg_esa.get_classification_areas()
+sdg_area_wikis = sdg_esa.get_classification_area_wikis()
+
 if preload_vectors:
     print("## Setup Research Areas ##")
-    research_areas_esa = ClassificationESA(esa_db_path, host, user, password, database_research_areas)
-    research_areas = research_areas_esa.get_classification_areas()
-    research_area_wikis = research_areas_esa.get_classification_area_wikis()
     research_area_vectors = research_areas_esa.get_classification_area_vectors()
 
     print("## Setup SDGs ##")
-    sdg_esa = ClassificationESA(esa_db_path, host, user, password, database_sdgs)
-    sdg_areas = sdg_esa.get_classification_areas()
-    sdg_area_wikis = sdg_esa.get_classification_area_wikis()
     sdg_area_vectors = sdg_esa.get_classification_area_vectors()
 
     preloaded = {
@@ -57,6 +59,19 @@ if preload_vectors:
             "classification_area_vectors": sdg_area_vectors
         }
     }
+else:
+    preloaded = {
+        "esa_research_areas": {
+            "classification_esa": research_areas_esa,
+            "classification_areas": research_areas,
+            "classification_area_wikis": research_area_wikis
+        },
+        "esa_sdgs": {
+            "classification_esa": sdg_esa,
+            "classification_areas": sdg_areas,
+            "classification_area_wikis": sdg_area_wikis
+        }
+    }
 print("done")
 
 
@@ -69,5 +84,7 @@ def get_classification_esa(classification_scheme):
                                                preloaded[classification_scheme]["classification_area_vectors"])
     else:
         classification_esa = ClassificationESA(esa_db_path, host, user, password, databases[classification_scheme],
+                                               preloaded[classification_scheme]["classification_areas"],
+                                               preloaded[classification_scheme]["classification_area_wikis"],
                                                load_all_vectors=False)
     return classification_esa
